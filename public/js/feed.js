626e86c50dde330ef3267cc57fbf33aac528223c
Evan Casey <evan.casey92@gmail.com>
2013-11-16 00:24:30 -0800

    put css back

--- public/js/feed.js
+++ public/js/feed.js
@@ -14,10 +14,10 @@ $(function(){
       
       // edit the template with the img metadata
       var url = data[i].url;
-      var templ = "<div class='img' id='id" + i + "' style='background: url(\"" + url + "\"); position: relative; width: 400px; height: 400px;'>" +
-                    "<div class='img-layer' style= 'display:none; position: absolute; top: 0; left: 0; width: 400px; height: 400px; background-color: #666565; opacity:0.5; font-size: 60px;'>" + 
-                      "<div class='icon ion-minus-circled' style='position: relative; left: 15%; top: 70%;'> </div> " +
-                      "<div class='icon ion-ios7-plus' style='position: relative; left: 55%; top: 70%;'> </div>" +                  
+      var templ = "<div class='img' id='id" + i + "' style='background: url(\"" + url + "\"); width: 400px; height: 400px;'>" +
+                    "<div class='img-layer' style= 'width: 400px; height: 400px;'>" + 
+                      "<div class='icon ion-minus-circled'> </div> " +
+                      "<div class='icon ion-ios7-plus'> </div>" +                  
                     "</div>" +
                   "</div>";    
 
