7edd41569a517705f3f9a1bdfdee28ee00a2f211
Sean McQueen <sean@mcqueen.net>
2013-11-16 03:12:58 -0800

    VOTING WORKS

--- routes/voting.js
+++ routes/voting.js
@@ -1,7 +1,31 @@
+require('../models/image.js');
+
+var mongoose = require('mongoose');
+var Img = mongoose.model('Image'); 
+
 /*
  * POST: up and downvoting images.
  */
 exports.vote = function(req, res){
 
-  res.json({'voted': 'chyeah'});
+  var id = req.body.id;
+  var vote = req.body.vote;
+
+  if (vote == 1) {
+    Img.update({instagramId: id}, {$inc: {"upvotes":1}}, function(err, image) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(image);
+      }
+    });
+  } else {
+    Img.update({instagramId: id}, {$inc: {"downvotes":1}}, function(err, image) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(image);
+      }
+    });
+  }
 };
