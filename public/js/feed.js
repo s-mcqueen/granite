821f89bd7b0e25b998750ce64a3f9d2bed449b6d
Evan Casey <evan.casey92@gmail.com>
2013-11-16 05:59:58 -0800

    sume stooaeu

--- public/js/feed.js
+++ public/js/feed.js
@@ -11,14 +11,22 @@ $(function(){
   // populates the image feed
   function populate(data) {
 
+    var img = $('.img');
+    if (img != null) {
+      $('.img').remove();
+    }
+
     // iterate through image objects and append to DOM
     var num_img = data.length;
     for (var i = 0; i < num_img; i++) {
       
       // edit the template with the img metadata
-      var url = data[i].url;
-      var id = data[i].id;
-      var templ = "<div class='img' id='id" + i + "' data-id='" + id + "' style='background: url(\"" + url + "\"); width: 320px; height: 320px;'>" +
+      var url = data[i].url,
+          id = data[i].id,
+          hashtag = data[i].hashtag,
+          user = data[i].user,
+          status = data[i].status;
+      var templ = "<div class='img' id='id" + i + "' data-id='" + id + "' data-hashtag='" + hashtag + "' data-user='" + user + "' data-status='" + status + "' style='background: url(\"" + url + "\"); width: 320px; height: 320px;'>" +
                     "<div class='img-layer' style= 'width: 320px; height: 320px;'>" + 
                       "<div class='icon ion-minus-circled'> </div> " +
                       "<div class='icon ion-ios7-plus'> </div>" +                  
@@ -65,7 +73,11 @@ $(function(){
     $(".img").on('click', function(e) {
       e.preventDefault();
 
-      var url = $(this).attr('style').split("url(\"")[1].split("\"")[0]; 
+      if ($(".modal-img") != null) {
+        $(".modal-img").remove();
+      }
+
+      var url = $(this).attr('style').split("url(\"")[1].split("\"")[0];
       var tempUrl = "<div class='modal-img' style='background: url(\"" + url + "\"); width: 600px; height: 600px;'>" +
                     "</div>";    
 
