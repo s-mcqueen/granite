2a699fd0ae1350f050231b80321e72f0a1639f1d
Remy Guercio <remyguercio@yahoo.com>
2013-11-15 23:51:24 -0800

    Added function to take instagram data and push to mongo.

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,12 +1,9 @@
 var request = require('request');
 
-/*
- * Routes for dealing with instagram go here.
- *
- */
+require('../models/image.js');
 
-
-
+var mongoose = require('mongoose'),
+  Img = mongoose.model('Image');
 
 /*
  * Perform handshake with instagram.
@@ -28,15 +25,44 @@ app.post('/callback', function(req, res){
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var data = JSON.parse(body).data;
-        for (var i = 0; i < data.length; i++) {
-          if (data[i].type == 'image') {
-            // data[i] is a photo obj
-            // data[i].id will be our database id to avoid duplicates
-          }
-        }
+        pushToMongo(data);
       }
     });
   // reply to let instagram know we didnt die
   res.send('Thanks');
 
 });
+/*
+* Takes instagram .data array from api call and pushes to mongo
+*
+*/
+function pushToMongo (argument) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].type == 'image') {
+      // get image ready for insert
+      var newImage = new Img({
+        instagramId : data[i].id,
+        instagramUsername : data[i].username,
+        instagramCreationTime : data[i].created_time,
+        caption : data[i].caption.text,
+        hashtags : data[i].tags,
+        smallRes : data[i].images.thumbnail.url,
+        mediumRes : data[i].images.low_resolution.url,
+        largeRes : data[i].images.standard_resolution.url,
+        lat : data[i].location.latitude,
+        lon : data[i].location.longitude,
+        upvotes : 0,
+        downvotes : 0
+      });
+        Img.find({id: data[i].id}, function(err, data){
+          // if there was no error and we didnt find anything go ahead and insert the new photo
+          if (!err && !data) {
+            // save it 
+            newImg.save(function (err, newImg) {
+              if (err) // TODO handle the error (not really important)
+            });
+          }
+        });   
+    }
+  }
+}
