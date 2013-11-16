8f4a1a2f8c4a3347609a4c298da3eaffdde279fe
Remy Guercio <remyguercio@yahoo.com>
2013-11-16 02:46:04 -0800

    Moved pushToMongo to util.js and updated instagram.js

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,12 +1,13 @@
 var request = require('request');
 var testData = require('../data/data1.json').data;
+var util = require('../utils/util.js');
 
 require('../models/image.js');
 var mongoose = require('mongoose');
 var Img = mongoose.model('Image');
 
 exports.test = function(req, res) {
-  pushToMongo(testData);
+  util.pushToMongo(testData);
 };
 
 /*
@@ -27,50 +28,10 @@ exports.instagramCallback = function(req, res){
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var data = JSON.parse(body).data;
-        pushToMongo(data, function(){});
+        util.pushToMongo(data, function(){});
       }
     });
   // reply to let instagram know we didnt die
   res.send('Thanks');
 };
-
-/*
-* Takes instagram .data array from api call and pushes to mongo
-*
-*/
-function pushToMongo (data, callback) {
-  for (var i = 0; i < data.length; i++) {
-    if (data[i].type == 'image') {
-
-      // get image ready for insert
-      var newImage = new Img({
-        instagramId : data[i].id,
-        instagramUsername : data[i].username,
-        instagramCreationTime : data[i].created_time,
-        caption : data[i].caption.text,
-        hashtags : data[i].tags,
-        smallRes : data[i].images.thumbnail.url,
-        mediumRes : data[i].images.low_resolution.url,
-        largeRes : data[i].images.standard_resolution.url,
-        upvotes : 0,
-        downvotes : 0
-      });
-
-      (function (imageToSave, innerCallback) {
-        Img.find({instagramId: data[i].id}, function(err, data){
-          // if there was no error and we didnt find anything go ahead and insert the new photo
-          if (!err && (data.length == 0)) {
-            imageToSave.save(function (err) {
-              innerCallback();
-              if (err) {
-                console.log(err);
-              }
-            });
-          }
-        });
-      })(newImage, callback);
-    }
-  }
-}
-
 
---
+++ utils/util.js
@@ -0,0 +1,42 @@
+require('../models/image.js');
+var mongoose = require('mongoose');
+var Img = mongoose.model('Image');
+
+/*
+* Takes instagram .data array from api call and pushes to mongo
+*
+*/
+exports.pushToMongo = function (data, callback) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].type == 'image') {
+
+      // get image ready for insert
+      var newImage = new Img({
+        instagramId : data[i].id,
+        instagramUsername : data[i].username,
+        instagramCreationTime : data[i].created_time,
+        caption : data[i].caption.text,
+        hashtags : data[i].tags,
+        smallRes : data[i].images.thumbnail.url,
+        mediumRes : data[i].images.low_resolution.url,
+        largeRes : data[i].images.standard_resolution.url,
+        upvotes : 0,
+        downvotes : 0
+      });
+
+      (function (imageToSave, innerCallback) {
+        Img.find({instagramId: data[i].id}, function(err, data){
+          // if there was no error and we didnt find anything go ahead and insert the new photo
+          if (!err && (data.length == 0)) {
+            imageToSave.save(function (err) {
+              innerCallback();
+              if (err) {
+                console.log(err);
+              }
+            });
+          }
+        });
+      })(newImage, callback);
+    }
+  }
+}
