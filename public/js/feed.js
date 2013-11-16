12aa92def27466f4fc4bc1bb4b00d2173c7cf6b1
Evan Casey <evan.casey92@gmail.com>
2013-11-16 00:10:27 -0800

    hover working

--- public/js/feed.js
+++ public/js/feed.js
@@ -12,22 +12,34 @@ $(function(){
     var num_img = data.length;
     for (var i = 0; i < num_img; i++) {
       
+      // edit the template with the img metadata
       var url = data[i].url;
-      var templ = "<div class=\"img\" style=\"background: url('" + url + "'); position: relative; width: 400px; height: 400px;\">" +
-                    "<div class=\"img-layer\" style= \"display:none; position: absolute; top: 0; left: 0; width: 400px; height: 400px; background-color: #666565; opacity:0.5; font-size: 60px;\">" + 
-                      "<div class=\"icon ion-minus-circled\" style=\"position: relative; left: 15%; top: 70%;\"> </div> " +
-                      "<div class=\"icon ion-ios7-plus\" style=\"position: relative; left: 55%; top: 70%;\"> </div>" +                  
+      var templ = "<div class='img' id='id" + i + "' style='background: url(\"" + url + "\"); position: relative; width: 400px; height: 400px;'>" +
+                    "<div class='img-layer' style= 'display:none; position: absolute; top: 0; left: 0; width: 400px; height: 400px; background-color: #666565; opacity:0.5; font-size: 60px;'>" + 
+                      "<div class='icon ion-minus-circled' style='position: relative; left: 15%; top: 70%;'> </div> " +
+                      "<div class='icon ion-ios7-plus' style='position: relative; left: 55%; top: 70%;'> </div>" +                  
                     "</div>" +
                   "</div>";    
 
-      // edit the template with the img metadata
+      // append the img templ to feed
       $('.feed').append(templ);
     }
 
+    // after this is finishes, enable hover effects
+    $('.img').mouseenter(function(e) {
+        e.preventDefault();        
 
+        $(this).children().show()
+        console.log("here");
+      });
+
+    $('.img').mouseleave(function(e) {
+      e.preventDefault();
+
+      $(this).children().hide()
+      console.log("here");
+    });
 
   }
-
-
 
 });    
